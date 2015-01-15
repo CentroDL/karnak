c2395cf41739c80b9555fc0e233da70a483a0189
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-15 14:59:36 -0500

    Adds additional routing to view streams by game

--- config.ru
+++ config.ru
@@ -3,6 +3,8 @@ require 'sinatra/base'
 require 'sinatra/contrib'
 require 'sinatra/reloader'
 require 'httparty'
+require 'uri'
+require 'pry'
 
 require_relative 'karnak'
 
--- karnak.rb
+++ karnak.rb
@@ -7,10 +7,17 @@ module Karnak
     end
 
     get '/' do
-      top_games_url = "https://api.twitch.tv/kraken/games/top?limit=10&offset=0"
+      top_games_url = "https://api.twitch.tv/kraken/games/top?limit=12&offset=0"
       top_games_hash = HTTParty.get top_games_url
-      @top_ten = top_games_hash["top"]
+      @top_games = top_games_hash["top"]
       erb :index
+    end
+
+    get '/games/:name' do
+      game_url = "https://api.twitch.tv/kraken/search/streams?" + URI.encode_www_form({"q" => params[:name]})
+      @game = HTTParty.get game_url
+      # binding.pry
+      erb :game
     end
 
 
