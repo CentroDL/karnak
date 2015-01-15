c338b3ab669e735bb0d40d0b7085e57d665b00de
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-15 00:03:39 -0500

    taught steve not to indent with tabs so barbie would love him again

--- karnak.rb
+++ karnak.rb
@@ -1,16 +1,18 @@
 module Karnak
-	class Server < Sinatra::Base
-		
-		configure :development do
-			register Sinatra::Reloader
-			$db = Redis.new
-		end
+  class Server < Sinatra::Base
 
-		get '/' do
-			top_games_url = "https://api.twitch.tv/kraken/games/top?limit=10&offset=0"
-			top_games_hash = HTTParty.get top_games_url
-			@top_ten = top_games_hash["top"]
-			erb :index
-		end
-	end
-end
+    configure :development do
+      register Sinatra::Reloader
+      $db = Redis.new
+    end
+
+    get '/' do
+      top_games_url = "https://api.twitch.tv/kraken/games/top?limit=10&offset=0"
+      top_games_hash = HTTParty.get top_games_url
+      @top_ten = top_games_hash["top"]
+      erb :index
+    end
+
+
+  end#Server
+end#Karnak
