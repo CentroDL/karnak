fb711b687cf115b6b748f95ebcce45cacf795061
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-16 15:36:09 -0500

    adds spike.rb for testing

---
+++ spike.rb
@@ -0,0 +1,17 @@
+require 'redis'
+require 'sinatra/base'
+require 'sinatra/contrib'
+require 'sinatra/reloader'
+require 'httparty'
+require 'uri'
+require 'pry'
+
+require_relative 'hitbox_helper'
+require_relative 'twitch_helper'
+
+require_relative 'server'
+
+include HitboxHelper
+include TwitchHelper
+
+binding.pry
