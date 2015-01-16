5f0658e4b36672fdaf09112cffa8e99e12ca6973
Dennis Liaw <dennis.liaw@gmail.com>
2015-01-16 14:27:14 -0500

    Improves hitbox.tv API method

--- hitbox_helper.rb
+++ hitbox_helper.rb
@@ -1,6 +1,6 @@
 module HitboxHelper
   def get_streams(game)
     media = JSON.parse HTTParty.get "http://api.hitbox.tv/media"
-    media["livestream"].select { |x| x["category_name"] == game }
+    media["livestream"].select { |x| x["category_name"] =~ /#{game}/i }
   end
 end
